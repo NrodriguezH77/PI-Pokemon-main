face9e6c4244dfa7fe165e723a2edca7f914b831
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2022-10-15 12:08:58 -0500

    deploy5

--- PI-Pokemon-main/api/index.js
+++ PI-Pokemon-main/api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  //chargeAndGetAllTypes()
+  chargeAndGetAllTypes()
   server.listen(3001, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });

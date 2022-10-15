917e7bdd11082488ed897217a26ae72c3e2af210
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2022-10-15 12:27:52 -0500

    deploy6

--- PI-Pokemon-main/api/src/db.js
+++ PI-Pokemon-main/api/src/db.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const pg = require('pg')
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -8,7 +9,8 @@ const {
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed,
+  dialectModule: pg
 });
 const basename = path.basename(__filename);
 

4c048ca8a43b1f0fb4d5399fbf32a9326025cdb8
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2023-01-01 23:17:18 -0500

    Configuraciones para las variables de entorno en client

--- PI-Pokemon-main/client/src/index.js
+++ PI-Pokemon-main/client/src/index.js
@@ -7,7 +7,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = "https://pokemon-app-dsyj.onrender.com" || "http://localhost:3001" ;
+const config = require('./config');
+const {
+  URL_BACK,
+} = config;
+axios.defaults.baseURL = /* "https://pokemon-app-dsyj.onrender.com" || */ URL_BACK ;
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>

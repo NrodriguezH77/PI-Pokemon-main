6b79dfc83adae677e55092a5b3a3478c0894dccb
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2023-01-25 11:47:38 -0500

    Fixed Apply error in Chrome

--- PI-Pokemon-main/client/src/redux/store.js
+++ PI-Pokemon-main/client/src/redux/store.js
@@ -2,10 +2,15 @@ import { createStore, applyMiddleware,  compose} from 'redux';
 import thunk from 'redux-thunk'
 import reducer  from '../redux/reducer'
 
+let middleware = [thunk]
 const store = createStore(reducer,
-    compose(
+    /* compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) */
+        compose(
+          applyMiddleware(...middleware),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+        )
       );
 
 export default store;
